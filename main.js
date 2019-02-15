28e523a7234d23b542d56c48732d3400822cbda7
Rodriguez-Mena <0280787@students.dadeschools.net>
2019-02-15 12:02:17 -0500

    finish css and logic

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// variable
+// VARIABLE
 let car = {
     make: 'Lambo',
     model: 'Aventado',
@@ -11,22 +11,53 @@ let car = {
      this.mileage = this.mileage + 10;
      alert(`New Mileage: ${this.mileage}`);
     },
-
+driveAroundUSA(){
+    alert(`Old Mileage: ${this.mileage}`);
+     this.mileage = this.mileage + 7000;
+     alert(`New Mileage: ${this.mileage}`);
+     alert(`car needs to tuneup`)
+     this.IsWorking = false;
+},
+getTuneUp(){
+    alert(`car is fixed and ready to go`)
+    this.isWorking = true;
+},
+honk(){
+    alert(`Honk! Honk!`);
+}
 }
 
-//Functions
+//FUNCTIONS
 // Logs all out car's current stats to the console
-const rewriteStats() => {
-
+const rewriteStats=() => {
+console.log(`make: ${car.make}`);
+console.log(`model: ${car.model}`);
+console.log(`color: ${car.color}`);
+console.log(`mileage: ${car.mileage}`);
+console.log(`isWorking: ${car.isWorking}`);
+console.log(``````````````````````````````````````)
 }
 
-//mainProcess
+//MAIN PROCESS
 //Captures keyboard input, Depending on the letter pressed it will  "call" (execute) different functions
- document.onkeyup(event) => {
+ document.onkeyup =  (event) => {
+
+//captures the keypress, converts it to lowercase, and save it to variable
     let letter = String.fromCharCode(event.keycode).toLowerCase();
 
-    if(letter === 'd'){
+if (letter === 'd'){
     car.driveToWork();
     rewriteStats();
-    }
+}
+if (letter === 'r'){
+ car.driveAroundUSA();
+ rewriteStats();
+        }
+ if (letter === 't'){
+car.getTuneUp();
+ rewriteStats();
+ } if (letter === 'h'){
+    car.honk();
+    rewriteStats();
+}
 }
